842e475935f784b66643ff23bf90eecdc04fbb48
Michael Kachi <jesmikky@gmail.com>
2023-06-15 04:46:01 +0100

    updates

--- app/config/database.php
+++ app/config/database.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Str;
 
 
-$jawsdb_url = env('JAWSDB_URL');
-$dbparts = parse_url($jawsdb_url);
-$hostname=$dbparts['host'];
-$username=$dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+// $jawsdb_url = env('JAWSDB_URL');
+// $dbparts = parse_url($jawsdb_url);
+// $hostname=$dbparts['host'];
+// $username=$dbparts['user'];
+// $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'],'/');
 return [
 
     /*
@@ -53,11 +53,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', $hostname),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -68,7 +68,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
