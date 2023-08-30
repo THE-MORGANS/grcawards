d90e6f5d5d15db282b212616858187911ad70685
Michael Ozoudeh <jesmikky@gmail.com>
2023-08-30 23:29:16 +0100

    updated error

--- app/resources/views/layouts/admin/master.blade.php
+++ app/resources/views/layouts/admin/master.blade.php
@@ -17,14 +17,13 @@
         <div class="content-page">
             <div class="content">
                 <!-- Topbar Start -->
-                @guest 
-                @else 
+              
                 @include('partials.admin.topbar')
                 <!-- end Topbar -->
 
                 <!-- Start Content-->
                 @yield('content')
-                @endguest
+                
                 <!-- container -->
             </div>
             <!-- Footer Start -->
