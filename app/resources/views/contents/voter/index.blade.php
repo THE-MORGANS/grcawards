1fce10b6ef2fbdafbaaa9b52e900b29e596443a8
Michael Ozoudeh <jesmikky@gmail.com>
2023-09-01 13:07:43 +0100

    changed dates

--- app/resources/views/contents/voter/index.blade.php
+++ app/resources/views/contents/voter/index.blade.php
@@ -95,7 +95,7 @@
 				<div class="conference-counter-wrap">
 					<img class="conference-counter-effect-1" src="assets/images/counter-icon-1.svg" alt="img">
 					<div class="conference-counter-cover">
-						<h4>Voting ends on the 21st August, 2023</h4>
+						<h4>Voting ends on the 15th September, 2023</h4>
 						{{-- <h3>Voting is Open</h3> --}}
 						 {{-- <h4>To view the shortlisted finalists, <a href="{{route('show_shortlisted_nomineees')}}" class="" tabindex="-1" style="color:rgba(39, 39, 39, .9)"><span>click here</span></a></h4>  --}}
 						{{-- <div id="clockdiv" class="clock-timer clock-timer-conference">
