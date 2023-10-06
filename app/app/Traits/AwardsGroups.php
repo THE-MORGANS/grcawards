12e0ef823154aa53260d35cc8959d38bae5b94de
Michael Ozoudeh <jesmikky@gmail.com>
2023-10-06 19:19:49 +0100

    updated

--- app/app/Traits/AwardsGroups.php
+++ app/app/Traits/AwardsGroups.php
@@ -7,7 +7,7 @@ trait AwardsGroups{
     {
         $data['award_group_one'] = [40,43]; //ComBankRiskComplainces
         $data['award_group_two'] = [55]; //com_bank_fraud_awarenesses
-        $data['award_group_three'] = [41,42,47,51,52,54,82,83,86,87,90,91]; //com_bank_chief_risk_officers
+        $data['award_group_three'] = [41,42,47,51,52,82,83,86,87,90,91]; //com_bank_chief_risk_officers
         $data['award_group_four'] = [44,45,46,49,53,54,84,85,88,89,92,93]; //grc_employers
         $data['award_group_five'] = [97]; //grc_solution_providers
         $data['award_group_six'] = [94]; //grc_training_providers
