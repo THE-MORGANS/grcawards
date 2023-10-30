23306d4fa580b60da95e969714b4d0021c829f10
Michael Ozoudeh <jesmikky@gmail.com>
2023-10-30 10:54:24 +0100

    update

--- app/app/Http/Controllers/JudgeController.php
+++ app/app/Http/Controllers/JudgeController.php
@@ -30,7 +30,8 @@ class JudgeController extends Controller
 
     public function ShowRegisteredUsers($award_program){
         $award_program_id = Hashids::connection('awardProgram')->decode($award_program);
-        $registered = SummitRegistration::select('email')->distint('email')->get();
+        $registered = SummitRegistration::latest()->distinct()->get();
+    
         return view('contents.admin.registered')->with(['registered' => $registered, 'award_program'=> $award_program_id]);
     }
 
