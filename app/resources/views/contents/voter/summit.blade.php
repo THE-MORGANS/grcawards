afada13295489e384a17d941e9b1dc9fa4274a98
Michael Ozoudeh <jesmikky@gmail.com>
2024-06-18 00:23:51 +0100

    updted date

--- app/resources/views/contents/voter/summit.blade.php
+++ app/resources/views/contents/voter/summit.blade.php
@@ -71,9 +71,9 @@
 					
 					<div class="col-12" style="text-align:justify;">
 						<ul class="mission-meta" style="display: block">
-							<li><i aria-hidden="true" class="mdi mdi-map-marker-outline mdi-18px"></i> Virtual / Online - Microsoft Teams</li> 
-							<li><i aria-hidden="true" class="uil uil-calendar-alt"></i>Tuesday, 23rd July, 2024</li>
-							<li><i class="mdi mdi-clock-outline"></i>12pm - 2pm</li> 
+							<li><i aria-hidden="true" class="mdi mdi-map-marker-outline mdi-18px"></i> Venue: Virtual / Online - Microsoft Teams</li> 
+							<li><i aria-hidden="true" class="uil uil-calendar-alt"></i>Date: Tuesday, 23rd July, 2024</li>
+							<li><i class="mdi mdi-clock-outline"></i>Time: 12pm - 2pm</li> 
 							<li><i class="mdi mdi-account-outline"></i> <a  href="{{route('summit_register')}}" target="_blank" style="font-size:20px"> REGISTER HERE </a> </li>
 							
 						</ul>
