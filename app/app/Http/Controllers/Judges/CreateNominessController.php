2442c152554975b4b2b3550da44a65e81e0ff3da
Michael Ozoudeh <jesmikky@gmail.com>
2024-10-10 15:38:28 +0100

    update

--- app/app/Http/Controllers/Judges/CreateNominessController.php
+++ app/app/Http/Controllers/Judges/CreateNominessController.php
@@ -16,34 +16,40 @@ class CreateNominessController extends Controller
         $data = $this->getAwardId();
         $award_program = Hashids::connection('awardProgram')->decode($award_program_id);
         // $award_hashids = Hashids::connection('award')->encode($award_id);
-        $award_hashid = Hashids::connection('award')->decode($award_id);
+        $award_hashid = Hashids::connection('award')->decode($award_id)[0];
         $votes = VoteCount::whereAwardId($award_hashid)->take(4)->orderBy('voteCount', 'DESC')->get();
         if (count($votes) > 0) {
-            if (in_array($award_hashid[0],  $data['award_group_one'])) {
+            if (in_array($award_hashid,  $data['award_group_one'])) {
                 $data =  $this->BankRiskComplainces($votes, $award_hashid);
                 return view('contents.admin.judge.ComBankRiskComplainces')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_two'])) {
+            } else if (in_array($award_hashid,  $data['award_group_two'])) {
                 $data = $this->BankFraudAwareness($votes, $award_hashid);
                 return view('contents.admin.judge.com_bank_fraud_awarenesses')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_three'])) {
+            } else if (in_array($award_hashid,  $data['award_group_three'])) {
                 $data =  $this->BankChiefRiskOfficer($votes, $award_hashid);
                 return view('contents.admin.judge.com_bank_chief_risk_officers')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_four'])) {
+            } else if (in_array($award_hashid,  $data['award_group_four'])) {
                 $data = $this->GrcEmployers($votes, $award_hashid);
                 return view('contents.admin.judge.grc_employers')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_five'])) {
+            } else if (in_array($award_hashid,  $data['award_group_five'])) {
                 $data = $this->GrcSolutionProviders($votes, $award_hashid);
                 return view('contents.admin.judge.grc_solution_providers')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_six'])) {
+            } else if (in_array($award_hashid,  $data['award_group_six'])) {
                 $data =  $this->GrcTrainingProvider($votes, $award_hashid);
                 return view('contents.admin.judge.grc_training_providers')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else if (in_array($award_hashid[0],  $data['award_group_seven'])) {
+            } else if (in_array($award_hashid,  $data['award_group_seven'])) {
                 $data = $this->GrcAntiFinCrimReporters($votes, $award_hashid);
                 return view('contents.admin.judge.grc_anti_fin_crim_reporters')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            }else if (in_array($award_hashid[0],  $data['award_group_eight'])) {
+            }else if (in_array($award_hashid,  $data['award_group_eight'])) {
                 $data = $this->crimePreventionAdvisoryServices($votes, $award_hashid);
                 return view('contents.admin.judge.crime_prevention_advisory_service')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
-            } else {
+            } else if(in_array($award_hashid,  $data['award_group_nine'])){
+                    $data =  $this->WomenInGrc($votes, $award_hashid);
+                return view('contents.admin.judge.women_in_grcs')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
+            } else if (in_array($award_hashid[0],  $data['award_group_ten'])) {
+                    $data = $this->Medias($votes, $award_hashid);
+                    return view('contents.admin.judge.medias')->with(['awards' => $data, 'nominessDetails' => '', 'award_program' => $award_program]);
+                } else {
                 $request->session()->flash('danger', 'Something went wrong, try again');
                 return back();
             }
