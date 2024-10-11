67f177aa74e287c2f8dba02420f7518d70eeb5ed
Michael Ozoudeh <jesmikky@gmail.com>
2024-10-11 12:40:07 +0100

    updates

--- app/app/Traits/JudgeVotes.php
+++ app/app/Traits/JudgeVotes.php
@@ -2,7 +2,7 @@
 namespace App\Traits;
 use App\Models\{ComBankChiefRiskOfficer, ComBankFraudAwareness,
     ComBankRiskComplaince, GrcAntiFinCrimReporter, GrcEmployer, 
-    GrcSolutionProvider, GrcTrainingProvider, CrimePreventionAdvisoryService, MediaVotes, WomenInGrc};
+    GrcSolutionProvider, GrcTrainingProvider, CrimePreventionAdvisoryService, GovernorsVotes, MediaVotes, WomenInGrc};
 use App\Models\VoteCount;
 use App\Models\JudgesVotes;
 
@@ -385,6 +385,41 @@ trait JudgeVotes {
         return $data;
     }
     
+    public function GovernorsJudgeVote($judges_votes, $nominee_ids, $award_hashid){
+        $check = JudgesVotes::where(['award_id'=>$award_hashid, 'judge_id' => auth('admin')->user()->id])->first();
+       $vote = [];
+        if($check){
+            $data = false;
+        }else{ 
+        foreach($nominee_ids as $nominess => $value){
+               $nominee = GovernorsVotes::where(['nominee_id' => $value, 'award_id'=>$award_hashid])->first();
+               if($nominee->judges_votes != null){    
+               $vote =  json_decode($nominee->judges_votes);
+                array_push($vote, $judges_votes[$nominess]);
+                $nominee->update(['judges_votes' => json_encode($vote)]);
+                JudgesVotes::create([
+                    'judge_id' => auth('admin')->user()->id,
+                     'votes' => $judges_votes[$nominess],
+                    'award_id' => $award_hashid, 
+                    'nominee_id' => $value,
+                ]);
+               }else{
+                 array_push($vote,$judges_votes[$nominess]);
+                 JudgesVotes::create([
+                    'judge_id' => auth('admin')->user()->id,
+                     'votes' => $judges_votes[$nominess],
+                    'award_id' => $award_hashid, 
+                    'nominee_id' => $value,
+                ]);
+                 $nominee->update(['judges_votes' => json_encode($vote)]);
+                
+               }
+               array_pop($vote);
+            }
+        $data = true;
+        }
+        return $data;
+    }
    
 }
 
--- app/app/Traits/NomineesAwards.php
+++ app/app/Traits/NomineesAwards.php
@@ -3,7 +3,7 @@ namespace App\Traits;
 use App\Models\VoteCount;
 use App\Models\{ComBankChiefRiskOfficer, ComBankFraudAwareness,
     ComBankRiskComplaince, GrcAntiFinCrimReporter, GrcEmployer, 
-    GrcSolutionProvider, GrcTrainingProvider, CrimePreventionAdvisoryService, MediaVotes, WomenInGrc};
+    GrcSolutionProvider, GrcTrainingProvider, CrimePreventionAdvisoryService, GovernorsVotes, MediaVotes, WomenInGrc};
 
 
 trait NomineesAwards {
@@ -230,7 +230,30 @@ public function WomenInGrc($votes, $award_hashid){
                 }
                         $data =  MediaVotes::whereAwardId($award_hashid)->get();
                         return $data;
-                        }
+         }
+
+          
+    public function GovernorsVotes($votes, $award_hashid){
+
+        $check =  GovernorsVotes::whereAwardId($award_hashid)->get();
+        if(count($check) > 0){
+           foreach($check as $cc => $val){
+             $val->fill(['award_id'=> $votes[$cc]->award_id,  'nominee_id'=>$votes[$cc]->nominee_id,'number_of_votes' => $votes[$cc]->voteCount, 
+              'percentage_votes' => ($votes[$cc]->voteCount)*(100/VoteCount::whereAwardId($award_hashid)->sum('voteCount')) ])->save();
+           }
+        }else{
+        foreach($votes as $vote){
+            GovernorsVotes::create([
+                'award_id' => $vote->award_id,
+                'nominee_id' => $vote->nominee_id,
+                'number_of_votes' => $vote->voteCount,
+                'percentage_votes' => ($vote->voteCount)*(100/VoteCount::whereAwardId($award_hashid)->sum('voteCount'))
+            ]);
+        }
+        }
+                $data =  GovernorsVotes::whereAwardId($award_hashid)->get();
+                return $data;
+ }
                         
             
 }
