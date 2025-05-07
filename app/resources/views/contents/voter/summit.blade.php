2d1d3af131f3de068bc0ec889b9395efe72f3e33
Michael Ozoudeh <jesmikky@gmail.com>
2025-05-07 07:20:09 +0100

    updated judges

--- app/resources/views/contents/voter/summit.blade.php
+++ app/resources/views/contents/voter/summit.blade.php
@@ -369,6 +369,29 @@
                     </div>
                 </div>
 
+                <div class="slide-our-speaker">
+                    <div class="our-speaker-item">
+                        <img src="{{asset('/assets/oseni.jpeg')}}"
+                            alt="img">
+                        <div class="speaker-item-info"
+                            style="position: relative;padding-left:15px;padding-right:15px;bottom:40px">
+                            <h3 class="name" style="font-size:18px">Dr. Ezekiel Oseni
+                            </h3>
+                            {{-- <p class="prof" style="font-size:16px;line-height:1rem;">(Keynote Speaker)</p> --}}
+                            <p class="prof mt-2" style="font-size:16px;line-height:1rem;">President & Chairman of Council Chartered Risk Management Institute of Nigeria || Ph.D., Business & Applied Economics || Professor of Accounting and Finance at Babcock University Business</p>
+                            <div class="meta">
+                                <span class="post-tag" style="margin-right:7px;">
+                                    <a href="https://www.linkedin.com/in/dr-ezekiel-oseni/" tabindex="-1"
+                                        target="_blank">
+                                        <i class="mdi mdi-linkedin mdi-24px" style="color:#fff"
+                                            aria-hidden="true"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
             <ul class="pt-5">
