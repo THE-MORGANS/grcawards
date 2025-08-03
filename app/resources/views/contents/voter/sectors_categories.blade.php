4d846ab7bb16c00b837ddad0fa279ae15e71c4ad
Michael Ozoudeh <jesmikky@gmail.com>
2025-08-04 00:11:41 +0100

    sadded criteria

--- app/resources/views/contents/voter/sectors_categories.blade.php
+++ app/resources/views/contents/voter/sectors_categories.blade.php
@@ -66,20 +66,16 @@
                                                         @foreach($sector->awards as $award)
                                                         <li>
                                                             <i class="mdi mdi-chevron-right"></i>
-                                                            {{$award->name}}
+                                                               <h6>{{$award->name}} </h6> 
                                                         </li>
-                                                      
-                                                        
+                                                            <li>  <span style="font-weight: 500px"> CRITERIA</span> {{$award->criteria}} </li>
                                                         @endforeach
-                                                          <h5> Nominees </h5>
+                                                             <h5> Nominees </h5>
                                                           @foreach ($sector->nominees as $nominee)
                                                           <li>
-                                                          {{$nominee->name}}
-                                                          </li>
                                                           @endforeach
                                                         @endif
                                                     </ul>
-
                                                 </div>
                                             </div>
                                         </div>
